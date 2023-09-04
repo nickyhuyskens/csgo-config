450baaae6a9a474ebb7bd0902b36929dd5ca5ef7
nickyhuyskens <nicky.huyskens@me.com>
2023-09-04 08:45:37 +0200

    Update cs2_config.bat

--- cs2_config.bat
+++ cs2_config.bat
@@ -1,15 +1,23 @@
 @ECHO OFF
 setlocal EnableExtensions EnableDelayedExpansion
-curl https://raw.githubusercontent.com/nickyhuyskens/csgo-config/main/cs2config.cfg -o "C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\game\csgo\cfg\autoexec.cfg"
-ECHO Downloaded autoexec.cfg to C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\game\csgo\cfg\autoexec.cfg
+
+rem Default download location
+set "DefaultDownloadLocation=C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\game\csgo\cfg"
+
+set /P "DownloadLocation=Enter the download location (press Enter for default location '%DefaultDownloadLocation%'): "
+if "%DownloadLocation%"=="" set "DownloadLocation=%DefaultDownloadLocation%"
+
+curl https://raw.githubusercontent.com/nickyhuyskens/csgo-config/main/cs2config.cfg -o "%DownloadLocation%"\autoexec.cfg
+ECHO Downloaded autoexec.cfg to %DownloadLocation%
+
 rem ...
-set "UserChoice=no"
-set /P "UserChoice=Do you want to open the file in notepad? (yes/no): "
-if "!UserChoice!" == "yes" or "!UserChoice!" == "y" (
-  notepad "C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\game\csgo\cfg\autoexec.cfg"
-)
-if not "!UserChoice!" == "yes" (
+
+choice /C YN /M "Do you want to open the file in notepad? (Y/N) [Y]: "
+if errorlevel 2 (
   echo "Ok bye."
   PAUSE
+) else (
+  notepad "%DownloadLocation%"
 )
+
 endlocal
